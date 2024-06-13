2e054b34adc6debb778ce0b07fa786fd9f02f55c
Tobias Keel <tobias.keel@bluewin.ch>
2024-06-13 14:03:53 +0200

    Basic timing with remote starter

--- TVBR_Timing/src/main.cpp
+++ TVBR_Timing/src/main.cpp
@@ -5,16 +5,25 @@
 
 #include <Arduino.h>
 
+#include <esp_now.h>
+#include <WiFi.h>
+
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define SCREEN_ADDRESS 0x3D
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
 #define MIN_TRIGGER_TIME 15
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+typedef struct struct_message {
+  char m;
+} struct_message;
+
+struct_message startMessage;
+
 int sw_mode_pin = 15; // High - Setup, Low - Race
-int sw_reset_pin = 2;
+int sw_reset_pin = 32;
 int light_gate_pin = 34;
 
 int led_pin = 19;
@@ -22,15 +31,12 @@ int led_pin = 19;
 int i2c_sda_pin = 21;
 int i2c_scl_pin = 22;
 
-int rx_pin = 16;
-int tx_pin = 17;
-
 int state;
 bool running = false;
 /*
-0 = setup
-1 = ready/display
+1 = ready
 2 = running
+3 = display
 */
 
 float times[20];
@@ -38,6 +44,22 @@ int times_i = 0;
 int start_time = 0;
 int falling_time = 0;
 int rising_time = 0;
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&startMessage, incomingData, sizeof(startMessage));
+  if(state == 1 && startMessage.m == 'S'){
+    start_time = millis();
+
+    for(int i = 0; i < 8; i++){
+      times[i] = 0.0;
+    }
+
+    times_i = 0;
+    rising_time = 0;
+    falling_time = 0;
+    state = 2;
+  }
+}
 
 void gate_change(){
   if(state == 2){
@@ -62,6 +84,17 @@ void display_ready(){
   display.setCursor(0, 0);
   display.cp437(true);
   display.println("READY");
+
+  display.display();
+}
+
+void display_error(){
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE); 
+  display.setCursor(0, 0);
+  display.cp437(true);
+  display.println("ERROR");
 
   display.display();
 }
@@ -120,6 +153,8 @@ void display_times(){
 
   Serial.println(times_i);
 
+  display.println("TIMES");
+
   for(int i = 0; i < times_i; i++){
     Serial.println(String(i+1) + ".  " + String(times[i], 3));
     display.println(String(i+1) + ".  " + String(times[i], 3));
@@ -130,43 +165,37 @@ void display_times(){
 
 void setup() {
   Serial.begin(9600);
-  Serial2.begin(9600, SERIAL_8N1, rx_pin, tx_pin);
 
-  pinMode(15, INPUT);
-  pinMode(2, INPUT);
-  pinMode(34, INPUT);
-
-  pinMode(led_pin, OUTPUT);
-
+  pinMode(sw_reset_pin, INPUT);
+  pinMode(light_gate_pin, INPUT);
   attachInterrupt(light_gate_pin, gate_change, CHANGE);
 
-  Wire.begin(21, 22);
+  Wire.begin(i2c_sda_pin, i2c_scl_pin);
   //if(!display.begin(SSD1306_EXTERNALVCC, SCREEN_ADDRESS)) {
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
+
   display_ready();
+
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+  }else{
+    if(esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv)) != ESP_OK){
+      Serial.println("Error registering ESP-NOW");
+    }
+  }
+ 
+  state = 1;
+  Serial.println("Setup done!...");
 }
 
 void loop() {
+  if(state == 1){ //ready
+    display_ready();
 
-  if(state != 2 && digitalRead(sw_mode_pin) == HIGH){
-    state = 0;
-    display_setup();
-  }else if(state != 2){
-    state = 1;
-    //display_ready();
-  }
-
-  
-  if(state == 0){ //Setup State
-    if(digitalRead(light_gate_pin) == HIGH){
-      digitalWrite(led_pin, LOW);
-    }else{
-      digitalWrite(led_pin, HIGH);
-    }
-  }else if(state == 1){
     if(digitalRead(sw_reset_pin) == HIGH){
       start_time = millis();
       
@@ -180,10 +209,11 @@ void loop() {
       state = 2;
       Serial.println("START");
     }
-  }else if(state == 2){
+  }else if(state == 2){ //running
+
     if(falling_time != 0 && rising_time != 0){
 
-      Serial.println("Cross ditected!");
+      Serial.println("Cross detected!");
       Serial.println(falling_time - rising_time);
       if(falling_time - rising_time  > MIN_TRIGGER_TIME){
         Serial.println("Valid!");
@@ -200,9 +230,15 @@ void loop() {
     }
 
     if(digitalRead(sw_reset_pin) == HIGH && millis() - start_time > 500){
+      state = 3;
+      delay(500);
+    }
+  }else if(state == 3){ //display
+    display_times();
+    
+    if(digitalRead(sw_reset_pin) == HIGH){
       state = 1;
-      display_times();
-      delay(1000);
+      delay(500);
     }
   }
 }
